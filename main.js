0d14f49e312ad1632b7eb31e6b0fad4b6aeb707d
Jerry Qian <jerry.qian@icsd.k12.ny.us>
2018-10-25 17:21:57 -0400

    Update

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+var elemPos = [];
+var elemPos2 = []
+
 $(document).ready(function() {
 
 	$('.expand-container').each(function() {
@@ -10,4 +13,48 @@ $(document).ready(function() {
 			p.slideToggle(500);
 		});
 	});
+
+	$('.line').each(function() {
+		elemPos.push({num: $(this).offset().top, elem: $(this)});
+	});
+
+	$('.lineN').each(function() {
+		elemPos2.push({num: $(this).offset().top, elem: $(this)});
+	})
+
+	console.log(elemPos);
+	console.log(elemPos2);
+
+	onScroll()
 });
+
+
+$(document).scroll(function() {
+	onScroll()
+});
+
+function onScroll() {
+	console.log('Scrolling');
+	elemPos.forEach(function(e) {
+		console.log('Element: ' + e);
+
+		if(e.num < scrollBottom()) {
+			e.elem.addClass('anim');
+			console.log('animating at ' + e.num + ' elem: ' + e.elem);
+		}
+	});
+
+	elemPos2.forEach(function(j) {
+		console.log('Element: ' + j)
+
+		if(j.num < scrollBottom()) {
+			console.log("should be adding animR")
+			j.elem.addClass('animR');
+			console.log('animating at ' + j.num + ' elem: ' + j.elem)
+		}
+	})
+}
+
+function scrollBottom() {
+	return $(window).scrollTop() + window.innerHeight;
+}
